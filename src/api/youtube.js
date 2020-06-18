073966be2d570b2c73d984a0bf52b84c0b1b1169
Diganta Som <itsdsom111@gmail.com>
2020-06-18 18:12:37 +0530

    fixed api key

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import url from './key';
 
 // the unique API key is from ->
 // https://console.developers.google.com/apis/api/youtube.googleapis.com/credentials?project=youtube-app-274810&folder=
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
+    baseURL: url,
 });
 
 // the API key is defined in App.js
