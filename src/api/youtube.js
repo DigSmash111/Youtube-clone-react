60808d72b73b509b2562858b279662e23d77d96d
Diganta Som <52863377+DigSmash111@users.noreply.github.com>
2020-06-18 18:13:21 +0530

    Update youtube.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import url from './key';
 
-// the unique API key is from ->
-// https://console.developers.google.com/apis/api/youtube.googleapis.com/credentials?project=youtube-app-274810&folder=
 export default axios.create({
     baseURL: url,
 });
